7af03d063563ea9a399c0e7bba8eae06327b0c4a
anhld7376-jpg <anhld7376@gmail.com>
2026-03-08 20:34:20 +0700

    main.py

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 
 WEBHOOK = "https://discord.com/api/webhooks/1480184925172011108/VHK2TDc3BYsvlqco-fKpCLYmcd0yqI9QVd_aOtwcxhVLP27ewgoKZmnaYFfCYWQPHP3u"
-PAGE = "https://mbasic.facebook.com/nghichthuyhanofficial"
+URL = "https://mbasic.facebook.com/nghichthuyhanofficial"
 
-last_post = None
+headers = {
+ "User-Agent": "Mozilla/5.0"
+}
 
-def send_discord(text, link):
-    data = {
-        "content": f"📢 Bài đăng Facebook mới\n{text}\n{link}"
-    }
-    requests.post(WEBHOOK, json=data)
+r = requests.get(URL, headers=headers)
+soup = BeautifulSoup(r.text,"html.parser")
 
-while True:
-    r = requests.get(PAGE)
-    soup = BeautifulSoup(r.text, "html.parser")
+post = soup.find("a", string="Full Story")
 
-    post = soup.find("a", href=True)
+if post:
+ link = "https://facebook.com"+post["href"]
 
-    if post:
-        link = "https://facebook.com" + post["href"]
-        text = post.text
-
-        global last_post
-
-        if link != last_post:
-            send_discord(text, link)
-            last_post = link
-
-    time.sleep(120)
+ requests.post(WEBHOOK,json={
+  "content":f"📢 Bài mới từ fanpage\n{link}"
+ })
